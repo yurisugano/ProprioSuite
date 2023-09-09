97b69fc8962e5a336ed52c8ea8249c51c3c9603b
[Yuri Vieira Sugano] <ysugano@uchicago.edu>
2023-09-09 20:19:38 -0300

    Create a separate module for smoothing methods

--- ProprioSuite/preprocessing/__init__.py
+++ ProprioSuite/preprocessing/__init__.py
@@ -1,2 +1,3 @@
 # __init__.py
 from .preprocessing import *
+from .smoothing_methods import *
--- ProprioSuite/preprocessing/preprocessing.py
+++ ProprioSuite/preprocessing/preprocessing.py
@@ -1,11 +1,9 @@
+from . import smoothing_methods
 import numpy as np
 import sqlite3
 import re
 import matplotlib.pyplot as plt
-from scipy.signal import savgol_filter, medfilt, convolve, gaussian
-from scipy.ndimage import gaussian_filter1d
-from scipy.spatial.distance import euclidean
-from scipy.stats import circmean
+
 
 
 def load_trial_data(trial_id, db_path):
@@ -53,108 +51,6 @@ def load_trial_data(trial_id, db_path):
         print("SQLite Error:", e)
         return None
     
-class SmoothingMethod:
-    def __init__(self, data):
-        self.time = data[:, 0]
-        self.x = data[:, 1]
-        self.y = data[:, 2]
-
-    def smooth(self):
-        raise NotImplementedError("Each smoothing method should implement this method.")
-
-    def adjust_time(self):
-        raise NotImplementedError("Each smoothing method should adjust the time accordingly.")
-
-
-class MovingAverage(SmoothingMethod):
-    def __init__(self, data, window_size):
-        super().__init__(data)
-        self.window_size = window_size
-
-    def smooth(self):
-        kernel = np.ones(self.window_size) / self.window_size
-        smoothed_x = convolve(self.x, kernel, mode='valid')
-        smoothed_y = convolve(self.y, kernel, mode='valid')
-        return smoothed_x, smoothed_y
-
-    def adjust_time(self):
-        adjusted_length = len(self.x) - self.window_size + 1
-        return self.time[-adjusted_length:]
-
-
-class WeightedMovingAverage(SmoothingMethod):
-    def __init__(self, data, window_size):
-        super().__init__(data)
-        self.window_size = window_size
-
-    def smooth(self):
-        weights = np.arange(1, self.window_size + 1)
-        smoothed_x = convolve(self.x, weights / weights.sum(), mode='valid')
-        smoothed_y = convolve(self.y, weights / weights.sum(), mode='valid')
-        return smoothed_x, smoothed_y
-
-    def adjust_time(self):
-        adjusted_length = len(self.x) - self.window_size + 1
-        return self.time[-adjusted_length:]
-
-
-class ExponentialMovingAverage(SmoothingMethod):
-    def __init__(self, data, alpha=0.3):
-        super().__init__(data)
-        self.alpha = alpha
-
-    def smooth(self):
-        smoothed_x = pd.Series(self.x).ewm(alpha=self.alpha).mean().values
-        smoothed_y = pd.Series(self.y).ewm(alpha=self.alpha).mean().values
-        return smoothed_x, smoothed_y
-
-    def adjust_time(self):
-        return self.time
-
-
-class GaussianSmoothing(SmoothingMethod):
-    def __init__(self, data, window_size):
-        super().__init__(data)
-        self.window_size = window_size
-
-    def smooth(self):
-        window = gaussian(self.window_size, self.window_size / 3)
-        smoothed_x = convolve(self.x, window / window.sum(), mode='valid')
-        smoothed_y = convolve(self.y, window / window.sum(), mode='valid')
-        return smoothed_x, smoothed_y
-
-    def adjust_time(self):
-        adjusted_length = len(self.x) - self.window_size + 1
-        return self.time[-adjusted_length:]
-
-
-class SavitzkyGolay(SmoothingMethod):
-    def __init__(self, data, window_size, polynomial_order=2):
-        super().__init__(data)
-        self.window_size = window_size
-        self.polynomial_order = polynomial_order
-
-    def smooth(self):
-        smoothed_x = savgol_filter(self.x, self.window_size, self.polynomial_order)
-        smoothed_y = savgol_filter(self.y, self.window_size, self.polynomial_order)
-        return smoothed_x, smoothed_y
-
-    def adjust_time(self):
-        return self.time
-
-
-class MedianFilter(SmoothingMethod):
-    def __init__(self, data, window_size):
-        super().__init__(data)
-        self.window_size = window_size
-
-    def smooth(self):
-        smoothed_x = medfilt(self.x, self.window_size)
-        smoothed_y = medfilt(self.y, self.window_size)
-        return smoothed_x, smoothed_y
-
-    def adjust_time(self):
-        return self.time
 
 
 class SmoothedData:
