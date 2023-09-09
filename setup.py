e7bde3fb81e7f9bdc10a798e4dcdb14511b90f56
[Yuri Vieira Sugano] <ysugano@uchicago.edu>
2023-09-09 17:07:04 -0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ProprioSuite',
+    version='0.1.0',
+    description='An open-source package tailored for two proprioceptive testing paradigms.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Yuri Vieira Sugano',
+    author_email='ysugano@uchicago.edu',
+    url='https://github.com/yurisugano/ProprioSuite',
+    license='MIT',
+    packages=find_packages(exclude=['tests*']),
+    install_requires=[
+        
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='proprioception, somatosensory, testing, analysis, computer vision',
+    python_requires='>=3.6',
+)
