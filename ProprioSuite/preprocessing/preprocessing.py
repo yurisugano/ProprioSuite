590d0fbddcb3322c0ad90cce72cc7c374d35770a
[Yuri Vieira Sugano] <ysugano@uchicago.edu>
2023-09-09 19:31:24 -0300

    Add smoothing function, graphing and summary statistics

--- ProprioSuite/preprocessing/preprocessing.py
+++ ProprioSuite/preprocessing/preprocessing.py
@@ -4,6 +4,8 @@ import re
 import matplotlib.pyplot as plt
 from scipy.signal import savgol_filter, medfilt, convolve, gaussian
 from scipy.ndimage import gaussian_filter1d
+from scipy.spatial.distance import euclidean
+from scipy.stats import circmean
 
 
 def load_trial_data(trial_id, db_path):
@@ -75,9 +77,8 @@ def median_filter(data, window_size):
     return medfilt(data, window_size)
 
 class SmoothedData:
-    def __init__(self, data, log_message):
+    def __init__(self, data):
         self.data = data
-        self.log_message = log_message
     
     def get_data(self):
         return self.data
@@ -85,13 +86,10 @@ class SmoothedData:
     def show_plot(self):
         plt.show()
     
-    def __repr__(self):
-        return self.log_message
-
 
 def smooth_data(raw_data, method, **kwargs):
     # Split the raw data into x and y coordinates
-    x, y = raw_data[:, 0], raw_data[:, 1]
+    x, y = raw_data[:, 1], raw_data[:, 2]
     
     # Map method names to functions
     methods = {
@@ -130,3 +128,59 @@ def smooth_data(raw_data, method, **kwargs):
     print(f"Data smoothed using {method} method.")
     
     return SmoothedData(smoothed_data)
+
+def xy_summaries(data):
+    # 1. Center the first frame at 0,0
+    centered_data = data.copy()
+    centered_data[:, 1] -= data[0, 1]
+    centered_data[:, 2] -= data[0, 2]
+    
+    # Compute distances between consecutive points for total_excursion and velocities
+    distances = [euclidean(centered_data[i, 1:], centered_data[i+1, 1:]) for i in range(len(centered_data)-1)]
+    total_excursion = sum(distances)
+    
+    # Compute time differences for velocity calculations
+    time_diffs = np.diff(centered_data[:, 0])
+    velocities = np.array(distances) / time_diffs
+    vel_mean = np.mean(velocities)
+    vel_max = np.max(velocities)
+    
+    # final_length and final_angle
+    final_point = centered_data[-1, 1:]
+    final_length = euclidean(final_point, [0, 0])
+    final_angle = np.degrees(np.arctan2(final_point[1], final_point[0]))
+    
+    # mean_angle
+    angles = np.arctan2(centered_data[:, 2], centered_data[:, 1])
+    mean_angle = np.degrees(circmean(angles))
+    
+    # x and y statistics
+    x_variance = np.var(centered_data[:, 1])
+    y_variance = np.var(centered_data[:, 2])
+    x_range = np.ptp(centered_data[:, 1])
+    y_range = np.ptp(centered_data[:, 2])
+    
+    # Quadrant percentages
+    q1 = len(centered_data[(centered_data[:, 1] >= 0) & (centered_data[:, 2] >= 0)]) / len(centered_data)
+    q2 = len(centered_data[(centered_data[:, 1] < 0) & (centered_data[:, 2] >= 0)]) / len(centered_data)
+    q3 = len(centered_data[(centered_data[:, 1] < 0) & (centered_data[:, 2] < 0)]) / len(centered_data)
+    q4 = len(centered_data[(centered_data[:, 1] >= 0) & (centered_data[:, 2] < 0)]) / len(centered_data)
+    
+    summaries = {
+        "total_excursion": total_excursion,
+        "vel_mean": vel_mean,
+        "vel_max": vel_max,
+        "final_length": final_length,
+        "final_angle": final_angle,
+        "mean_angle": mean_angle,
+        "x_variance": x_variance,
+        "y_variance": y_variance,
+        "x_range": x_range,
+        "y_range": y_range,
+        "pct_1q": q1 * 100,
+        "pct_2q": q2 * 100,
+        "pct_3q": q3 * 100,
+        "pct_4q": q4 * 100,
+    }
+    
+    return summaries
